da118a560f83fa745815b3e1d5c473b8f3923b34
Egor <egorvlz2013@yandex.ru>
2020-12-15 15:53:55 +0300

    Добавлены ассоциации для моделей

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions, dependent: :destroy
+  has_many :user_tests, dependent: :destroy
+  has_many :passing_users, through: :user_tests
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :category
+
   def self.names_by_category(category)
     joins('INNER JOIN categories ON categories.id = tests.category_id')
     .where(categories: { title: category } ).order(:title :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_tests, dependent: :destroy
+  has_many :created_tests
+
   def completed_tests_by_level(level)
     Test.joins('INNER JOIN user_tests ON user_tests.test_id = tests.id')
         .where(tests: { level: level }, user_tests: { user_id: id })
