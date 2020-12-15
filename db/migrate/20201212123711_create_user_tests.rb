7acfea0c2f9e0b1ab75264b52642e78ac40b8feb
Egor <egorvlz2013@yandex.ru>
2020-12-15 11:13:45 +0300

    refactor

--- db/migrate/20201212123711_create_user_tests.rb
+++ db/migrate/20201212123711_create_user_tests.rb
@@ -1,8 +1,8 @@
 class CreateUserTests < ActiveRecord::Migration[5.2]
   def change
     create_table :user_tests do |t|
-      t.references :user, foreign_key: true
-      t.references :test, foreign_key: true
+      t.references :user, foreign_key: true, null: false
+      t.references :test, foreign_key: true, null: false
 
       t.timestamps
     end
