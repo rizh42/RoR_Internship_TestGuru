e9aa535cb042951d9e2b79467831f916f735363b
Egor <egorvlz2013@yandex.ru>
2021-02-08 15:51:59 +0300

    Стандартные действия с видами для вопросов

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 users = User.create!([
-                        { name: 'user1', email: 'ex1@ex.ru' },
-                        { name: 'user2', email: 'ex2@ex.ru' },
-                        { name: 'user3', email: 'ex3@ex.ru' }
-                      ])
+                       { name: 'user1', email: 'ex1@ex.ru' },
+                       { name: 'user2', email: 'ex2@ex.ru' },
+                       { name: 'user3', email: 'ex3@ex.ru' }
+                     ])
 
 cat = Category.create!([
-                           { title: 'Ruby' },
-                           { title: 'ML' },
-                           { title: 'HTML' },
-                           { title: 'C++' }
-                         ])
+                         { title: 'Ruby' },
+                         { title: 'ML' },
+                         { title: 'HTML' },
+                         { title: 'C++' }
+                       ])
 
 tests = Test.create!([
                        { title: 'First',
@@ -31,22 +31,22 @@ tests = Test.create!([
                      ])
 
 quest = Question.create!([
-                            { title: 'First', test: tests[0] },
-                            { title: 'Second', test: tests[1] },
-                            { title: 'Third', test: tests[2] },
-                            { title: 'Fourth', test: tests[3] }
-                          ])
+                           { title: 'First', test: tests[0] },
+                           { title: 'Second', test: tests[1] },
+                           { title: 'Third', test: tests[2] },
+                           { title: 'Fourth', test: tests[3] }
+                         ])
 
 Answer.create!([
-                  { title: '+', question: quest[0] },
-                  { title: '-', correct: true, question: quest[1] },
-                  { title: '++', question: quest[2] },
-                  { title: '--', correct: true, question: quest[3] }
-                ])
+                 { title: '+', question: quest[0] },
+                 { title: '-', correct: true, question: quest[1] },
+                 { title: '++', question: quest[2] },
+                 { title: '--', correct: true, question: quest[3] }
+               ])
 
 UserTest.create!([
-                    { user: users[0], test: tests[0] },
-                    { user: users[1], test: tests[1] },
-                    { user: users[2], test: tests[2] },
-                    { user: users[0], test: tests[3] }
-                  ])
+                   { user: users[0], test: tests[0] },
+                   { user: users[1], test: tests[1] },
+                   { user: users[2], test: tests[2] },
+                   { user: users[0], test: tests[3] }
+                 ])
